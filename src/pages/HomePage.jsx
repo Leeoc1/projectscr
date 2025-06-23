2f46be3fc56136796f0684a7b82c71982d1d85c7
Kimby0611 <bycamp0604@gmail.com>
2025-06-23 12:47:48 +0900

    .

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Header from "../pubcomponent/Header";
-import ImageSlide from "../components/homepagecomponents/ImageSlide";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
-      <ImageSlide />
+      <h2>HomePage</h2>
     </div>
   );
 };

7fedeb9f908e7a772d0e0c588d611bb625032264
Yoojin-J <syoojin03@gmail.com>
2025-06-23 12:19:48 +0900

    v0
    기본설정

---
+++ src/components/homepagecomponents/ImageSlide.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ImageSlide = () => {
+  return (
+    <div>
+      <h2>ImageSlide</h2>
+    </div>
+  );
+};
+
+export default ImageSlide;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../pubcomponent/Header";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <h2>HomePage</h2>
+    </div>
+  );
+};
+
+export default HomePage;

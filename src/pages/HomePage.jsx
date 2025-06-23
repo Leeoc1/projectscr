83ccf52a0c6a9baab6a4fe0cad6e35dbf4587d4e
Kimby0611 <bycamp0604@gmail.com>
2025-06-23 12:04:27 +0900

    component분리작업 완료
    더 나누고 싶으면 얘기하세요....................

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import ImageSlide from "../component/homecomponent/ImageSlide";
+import Header from "../pubcomponent/Header";
 
 const HomePage = () => {
   return (
     <div>
+      <Header />
       <h2>HomePage</h2>
+      <ImageSlide />
     </div>
   );
 };

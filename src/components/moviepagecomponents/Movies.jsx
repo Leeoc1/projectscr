8d08917fac911a5abccfa04604b4f6a5acf85488
Kimby0611 <bycamp0604@gmail.com>
2025-06-23 12:10:03 +0900

    .

---
+++ src/components/moviepagecomponents/Movies.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Movies = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <img src="" alt="" className="movie1" />
+          <label htmlFor=""></label>
+        </div>
+        <img src="" alt="" className="movie2" />
+        <img src="" alt="" className="movie3" />
+        <img src="" alt="" className="movie4" />
+      </div>
+      <img src="" alt="" className="movie5" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+    </div>
+  );
+};
+
+export default Movies;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ImageSlide from "../component/homecomponent/ImageSlide";
+import Header from "../pubcomponent/Header";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <h2>HomePage</h2>
+      <ImageSlide />
+    </div>
+  );
+};
+
+export default HomePage;

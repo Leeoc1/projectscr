d52d56bf0ab2a6375a97c0c0d49a8452d00a201d
Kimby0611 <bycamp0604@gmail.com>
2025-06-24 09:17:56 +0900

    헤더, 슬라이드

--- src/pubcomponent/Header.jsx
+++ src/pubcomponent/Header.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import "../pubcomponentcss/Header.css";
 
 const Header = () => {
   return (
     <div>
-      <h2>Header</h2>
+      <div className="header">
+        <div className="header_content">
+          <div className="contents">
+            <div className="logo">
+              <img src="/images/logo.png" alt="The CINEMA" />
+            </div>
+
+            <ul className="memberinfo">
+              <li>로그인</li>
+              <li>회원가입</li>
+              <li>고객센터</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="nav">
+          <div className="contents">
+            <ul className="menu">
+              <li>영화</li>
+              <li>극장</li>
+              <li>예매</li>
+              <li>이벤트</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

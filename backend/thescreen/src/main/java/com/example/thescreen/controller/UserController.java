2fd080347d0bc9664abc5251c62c009302c2ceb9
joannes0 <joannes1837@gmail.com>
2025-06-30 23:45:57 +0900

    063025 23:45

--- backend/thescreen/src/main/java/com/example/thescreen/controller/UserController.java
+++ backend/thescreen/src/main/java/com/example/thescreen/controller/UserController.java
@@ -16,17 +16,7 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-//    @GetMapping("/list")
-//    public String listUsers(Model model) {
-//        List<User> users = userRepository.findAll();
-//        model.addAttribute("users", users);
-//        model.addAttribute("title", "사용자 목록");
-//        return "user/userlist";
-//    }
-
     @GetMapping("/list")
-    // @org.springframework.web.bind.annotation.ResponseBody
-    @ResponseBody
     public List<User> getUsersApi() {
         return userRepository.findAll();
     }

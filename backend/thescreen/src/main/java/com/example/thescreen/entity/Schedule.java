08a5874b4ae246ab3f9f8c1ee08f84c90ab5c901
Kimby0611 <bycamp0604@gmail.com>
2025-07-01 22:45:48 +0900

    이거 수정 필요

--- backend/thescreen/src/main/java/com/example/thescreen/entity/Schedule.java
+++ backend/thescreen/src/main/java/com/example/thescreen/entity/Schedule.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "schedule")
 @Getter
+@Setter
 @NoArgsConstructor
 public class Schedule {
 

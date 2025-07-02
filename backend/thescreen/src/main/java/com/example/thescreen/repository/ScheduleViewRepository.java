f1cc54746232ea6a55594981da1273bb2c5c492f
Kimby0611 <bycamp0604@gmail.com>
2025-07-02 19:17:50 +0900

    .

--- backend/thescreen/src/main/java/com/example/thescreen/repository/ScheduleViewRepository.java
+++ backend/thescreen/src/main/java/com/example/thescreen/repository/ScheduleViewRepository.java
@@ -1,7 +1,7 @@
-//package com.example.thescreen.repository;
-//
-//import com.example.thescreen.entity.ScheduleView;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ScheduleViewRepository extends JpaRepository<ScheduleView, String> {
-//}
+package com.example.thescreen.repository;
+
+import com.example.thescreen.entity.ScheduleView;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleViewRepository extends JpaRepository<ScheduleView, String> {
+}
--- front/src/api/api.js
+++ front/src/api/api.js
@@ -33,7 +33,7 @@ export const getMovieList = () =>
 // 극장 목록 조회
 export const getCinemas = () =>
   api
-    .get("/cinema")
+    .get("/cinemas")
     .then((response) => response.data)
     .catch((error) => {
       console.error("Error fetching cinema:", error);

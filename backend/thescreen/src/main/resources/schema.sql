ae5fba55541f8f938587fb74a2632b78bf0e1c51
Kimby0611 <bycamp0604@gmail.com>
2025-07-02 19:35:49 +0900

    수정중

--- backend/thescreen/src/main/resources/schema.sql
+++ backend/thescreen/src/main/resources/schema.sql
@@ -1,16 +1,16 @@
-CREATE OR REPLACE VIEW screen_view AS
-SELECT
-    r.regioncd,
-    r.regionnm,
-    c.cinemacd,
-    c.cinemanm,
-    s.screencd,
-    s.screenname,
-    s.seatcount
-FROM
-    region r
-    INNER JOIN cinema c ON r.regioncd = c.regioncd
-    INNER JOIN screen s ON c.cinemacd = s.cinemacd
-WHERE
-    s.screencd LIKE 'SCR%'
-    AND CAST(SUBSTRING(s.screencd FROM 4) AS INTEGER) <= 10;
+--CREATE OR REPLACE VIEW screen_view AS
+--SELECT
+--    r.regioncd,
+--    r.regionnm,
+--    c.cinemacd,
+--    c.cinemanm,
+--    s.screencd,
+--    s.screenname,
+--    s.seatcount
+--FROM
+--    region r
+--    INNER JOIN cinema c ON r.regioncd = c.regioncd
+--    INNER JOIN screen s ON c.cinemacd = s.cinemacd
+--WHERE
+--    s.screencd LIKE 'SCR%'
+--    AND CAST(SUBSTRING(s.screencd FROM 4) AS INTEGER) <= 10;

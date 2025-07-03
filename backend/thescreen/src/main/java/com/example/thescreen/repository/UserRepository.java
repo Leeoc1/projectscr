fd0e29b36c84aa532cdc3444d53d8e2d71c5ce02
Leeoc1 <lch5752@gmail.com>
2025-07-03 15:23:07 +0900

    a

--- backend/thescreen/src/main/java/com/example/thescreen/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.thescreen.repository;
-
-import com.example.thescreen.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> { // User 엔티티 리포지토리
-}

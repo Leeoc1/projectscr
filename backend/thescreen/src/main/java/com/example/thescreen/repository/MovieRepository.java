892b58ae2efaaf8f428fe3702551ed8991a4fb81
Kimby0611 <bycamp0604@gmail.com>
2025-07-04 23:58:56 +0900

    java버전 집이랑 학원이랑 다름

--- backend/thescreen/src/main/java/com/example/thescreen/repository/MovieRepository.java
+++ backend/thescreen/src/main/java/com/example/thescreen/repository/MovieRepository.java
@@ -3,6 +3,7 @@ package com.example.thescreen.repository;
 import com.example.thescreen.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 

b881a752b94805f9708f33d533906d45170c5886
Yoojin-J <syoojin03@gmail.com>
2025-07-10 16:20:39 +0900

    .

--- backend/thescreen/src/main/java/com/example/thescreen/repository/ReservationViewRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.thescreen.repository;
-
-
-import com.example.thescreen.entity.ReservationView;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationViewRepository extends JpaRepository<ReservationView, String> {
-}

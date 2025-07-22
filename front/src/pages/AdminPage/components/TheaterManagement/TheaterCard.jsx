ec2a650c12f5d86861a47c1036f29c3ad338af45
Leeoc1 <lch5752@gmail.com>
2025-07-22 14:38:30 +0900

    07 22

--- front/src/pages/AdminPage/components/TheaterManagement/TheaterCard.jsx
+++ front/src/pages/AdminPage/components/TheaterManagement/TheaterCard.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import MovieRegistrationModal from "./MRModal/MovieRegistrationModal";
 
-const TheaterCard = ({ theater, onDetailClick, onEdit, onDelete }) => {
+const TheaterCard = ({ theater, onDetailClick, onDelete }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleMovieRegistration = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div className="thm-theater-card">
       <h3>{theater.cinemanm}</h3>
@@ -13,16 +24,20 @@ const TheaterCard = ({ theater, onDetailClick, onEdit, onDelete }) => {
         </p>
       </div>
       <div className="thm-theater-actions">
-        <button className="adp-btn-edit" onClick={() => onEdit(theater)}>
-          수정
-        </button>
         <button className="adp-btn-view" onClick={onDetailClick}>
           상세보기
+        </button>
+        <button className="adp-btn-edit" onClick={handleMovieRegistration}>
+          영화 등록
         </button>
         <button className="adp-btn-delete" onClick={() => onDelete(theater)}>
           삭제
         </button>
       </div>
+
+      {isModalOpen && (
+        <MovieRegistrationModal theater={theater} onClose={handleModalClose} />
+      )}
     </div>
   );
 };

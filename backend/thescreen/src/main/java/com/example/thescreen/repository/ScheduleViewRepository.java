45383cd72425068e1b19b973b726a433c49934b8
Leeoc1 <lch5752@gmail.com>
2025-07-25 10:00:09 +0900

    1

--- backend/thescreen/src/main/java/com/example/thescreen/repository/ScheduleViewRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.thescreen.repository;
-
-import com.example.thescreen.entity.ScheduleView;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleViewRepository extends JpaRepository<ScheduleView, String> {
-}

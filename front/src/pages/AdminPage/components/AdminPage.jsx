115b871e5bc872d54230bde9818d66e0a6cb16c9
Leeoc1 <lch5752@gmail.com>
2025-07-30 11:31:26 +0900

    어드민페이지 정리 완료

--- front/src/pages/AdminPage/components/AdminPage.jsx
+++ front/src/pages/AdminPage/components/AdminPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import SalesOverview from "./adminmain/SalesOverview";
 import StaffManagement from "./StaffManagement/StaffManagement";
